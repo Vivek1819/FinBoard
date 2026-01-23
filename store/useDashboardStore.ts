9055e00c5e00378612a489603c8dd267c644334c
Vivek Hipparkar <vivekhipparkar@gmail.com>
2026-01-23 11:01:29 +0530

    Dashboard Persist added

--- store/useDashboardStore.ts
+++ store/useDashboardStore.ts
@@ -10,21 +10,13 @@ type DashboardState = {
   removeWidget: (id: string) => void;
   clearWidgets: () => void;
   updateWidget: (id: string, updater: (w: WidgetConfig) => WidgetConfig) => void;
-
   reorderWidgets: (oldIndex: number, newIndex: number) => void;
 };
-
 
 export const useDashboardStore = create<DashboardState>()(
   persist(
     (set) => ({
-
       widgets: [],
-
-      reorderWidgets: (oldIndex, newIndex) =>
-        set((state) => ({
-          widgets: arrayMove(state.widgets, oldIndex, newIndex),
-        })),
 
       addWidget: (widget) =>
         set((state) => ({
@@ -43,10 +35,19 @@ export const useDashboardStore = create<DashboardState>()(
           widgets: state.widgets.map((w) =>
             w.id === id ? updater(w) : w
           ),
-        }))
+        })),
+
+      reorderWidgets: (oldIndex, newIndex) =>
+        set((state) => ({
+          widgets: arrayMove(state.widgets, oldIndex, newIndex),
+        })),
     }),
     {
       name: "finboard-dashboard",
+      version: 1,
+      partialize: (state) => ({
+        widgets: state.widgets,
+      }),
     }
   )
 );
